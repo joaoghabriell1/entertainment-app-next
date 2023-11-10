2834b22fbbe94920d62ba96764232da10c35f77c
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-11-09 22:21:41 -0300

    deploy

--- src/app/tvshows/page.tsx
+++ src/app/tvshows/page.tsx
@@ -6,6 +6,7 @@ const TvShows = () => {
   return (
     <section className="min-h-screen bg-slate-900 text-white pb-4 lg:pl-28">
       <SearchBar />
+      <PopularTvShows />
       <TrendingTvShows />
     </section>
   );
